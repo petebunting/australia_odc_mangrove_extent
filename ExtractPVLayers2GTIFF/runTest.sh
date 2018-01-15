29c80145520d050b76506fd4a788ec2703ada617
Pete Bunting <petebunting@mac.com>
2018-01-15 11:50:54 +1100

    Update with fix ROI extents.

--- ExtractPVLayers2GTIFF/runTest.sh
+++ ExtractPVLayers2GTIFF/runTest.sh
@@ -1,9 +1,9 @@
-python ExtractAnnualPVData.py --minlat -17.6 --maxlat -17.5 --minlon 140.6 --maxlon 140.8 --startyear 1987 --endyear 2016 --outimg /home/552/pjb552/MangroveExtentImg.tif
+#python ExtractAnnualPVData.py --minlat -17.6 --maxlat -17.5 --minlon 140.6 --maxlon 140.8 --startyear 1987 --endyear 2016 --outimg /home/552/pjb552/PVFromCubeImg.tif
 
-python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 131.2 --startyear 2014 --endyear 2014 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_2014.tif
+python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 132.9 --startyear 2014 --endyear 2014 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_2014.tif
 
-python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 131.2 --startyear 1991 --endyear 1991 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_1991.tif
+python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 132.9 --startyear 1991 --endyear 1991 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_1991.tif
 
-python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 131.2 --startyear 2002 --endyear 2002 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_2002.tif
+python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 132.9 --startyear 2002 --endyear 2002 --outimg /home/552/pjb552/PVFromCube_KakaduRegion_2002.tif
 
-python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --maxlon 131.2 --startyear 2002 --endyear 2002 --outimg /home/552/pjb552/PVFromCube_LeichardtRegion_2011.tif
+python ExtractAnnualPVData.py --minlat -17.7 --maxlat -17.5 --minlon 139.7 --maxlon 139.9 --startyear 2011 --endyear 2011 --outimg /home/552/pjb552/PVFromCube_LeichardtRegion_2011.tif
