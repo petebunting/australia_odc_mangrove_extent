42d0825c6ad10568c602b95680ff059259d11492
Pete Bunting <petebunting@mac.com>
2018-01-15 21:45:43 +1100

    Added script to run with conversion of PV to canopy cover

---
+++ CalcMangroveChangeAnnualCC/runTest.sh
@@ -0,0 +1,2 @@
+python CalcMangroveChangeWithAnnualFC.py --minlat -17.6 --maxlat -17.5 --minlon 140.6 --maxlon 140.8 --startyear 1987 --endyear 2016 --pvfcthres 30 --outimg /home/552/pjb552/MangroveExtentImg.tif --outstats /home/552/pjb552/MangroveExtentStats.csv
+
