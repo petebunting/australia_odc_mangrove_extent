7e59dd1bbc8259ca40305ef1cdca71b4f864fa3c
Pete Bunting <petebunting@mac.com>
2018-01-15 11:27:18 +1100

    Script to download lidar from dropbox.

---
+++ CanopyCover/DwnloadData/Dwnld_GulfCHM.sh
@@ -0,0 +1,3 @@
+curl -L https://www.dropbox.com/sh/flw3qrm8bs943ok/AAByHRiNQ7aI29pFsgPXqUjTa?dl=1 > CHM_0.5m.zip
+
+curl -L https://www.dropbox.com/sh/w4402hukmlccfiz/AACmJm0Q0zT_32_9BanY5_GZa?dl=1 > 0902Q6_Albert_6_pit-free_CHM_0.5m.zip
