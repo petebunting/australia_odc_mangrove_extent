f70a053e477a131689e80f1a7462eec3b24a7c32
Pete Bunting <petebunting@mac.com>
2018-01-17 10:40:23 +1100

    Add another region to export

--- ExtractPVLayers2GTIFF/runTest.sh
+++ ExtractPVLayers2GTIFF/runTest.sh
@@ -9,3 +9,5 @@ python ExtractAnnualPVData.py --minlat -12.8 --maxlat -11.8 --minlon 131.9 --max
 python ExtractAnnualPVData.py --minlat -17.7 --maxlat -17.5 --minlon 139.7 --maxlon 139.9 --startyear 2011 --endyear 2011 --outimg /home/552/pjb552/PVFromCube_LeichardtRegion_2011.tif
 
 python ExtractAnnualPVData.py --minlat -20.1 --maxlat -19.9 --minlon 119.5 --maxlon 119.8 --startyear 2015 --endyear 2015 --outimg /home/552/pjb552/PVFromCube_WAFieldData_2015.tif
+
+python ExtractAnnualPVData.py --minlat -15.8 --maxlat -15.2 --minlon 124.2 --maxlon 125.3 --startyear 2016 --endyear 2016 --outimg /home/552/pjb552/PVFromCube_WAFieldData_2016.tif
