700f97f442f2526b7e83eba4724e0841b8b51455
Pete Bunting <petebunting@mac.com>
2018-01-19 14:51:35 +1100

    Update with visualisation scripts.

--- Visualisation/CreateMangChangeVisual.py
+++ Visualisation/CreateMangChangeVisual.py
@@ -3,21 +3,27 @@ import numpy
 import matplotlib.pyplot as plt
 
 
-tilesOfInterest = [970, 971, 972, 1211, 1212, 1213]#[1657]#[664, 665, 666, 668, 670, 901, 902]#[2044, 2045, 2046, 2047, 2048, 1795, 1796, 1797, 1798, 1550, 1551, 1552]
+tilesOfInterest = numpy.arange(1, 2380, 1)#[970, 971, 972, 1211, 1212, 1213]#[1657]#[664, 665, 666, 668, 670, 901, 902]#[2044, 2045, 2046, 2047, 2048, 1795, 1796, 1797, 1798, 1550, 1551, 1552]
 
 outTotalPlotFile = 'MangTotalAreaPlot.pdf'
 outTypePlotFile = 'MangTypeAreaPlot.pdf'
 
-data = pandas.read_pickle("MangChangePVFC_V2_1987_to_2016.pkl.gz", compression="gzip")
+data = pandas.read_pickle("MangChangePVFC_V2.1_1987_to_2016.pkl.gz", compression="gzip")
 
 outData = data[tilesOfInterest[0]][['total','low','mid','high']]
 if len(tilesOfInterest) > 1:
     for tile in tilesOfInterest[1:]:
         outData = outData + data[tile][['total','low','mid','high']]
 
-ax=outData['total'].plot(x=outData.index, y=outData['total'], legend=False)
-ax.set_ylim(0, outData['total'].values.max()*1.1)
+outData = (outData * 625) / 1000000
+
+ax1=outData['total'].plot(x=outData.index, y=outData['total'], legend=False)
+ax1.set_ylim(0, outData['total'].values.max()*1.1)
+ax1.set_xlabel('Time (Years)')
+ax1.set_ylabel('Area (km$^{2}$)')
 plt.savefig(outTotalPlotFile)
 
-outData[['low','mid','high']].plot.bar(stacked=True, figsize=(10,7), color=['#9FFF4C', '#5ECC00', '#3B7F00'], legend=False, width=1.0)
+ax = outData[['low','mid','high']].plot.bar(stacked=True, figsize=(10,7), color=['#9FFF4C', '#5ECC00', '#3B7F00'], legend=False, width=1.0)
+ax.set_xlabel('Time (Years)')
+ax.set_ylabel('Area (km$^{2}$)')
 plt.savefig(outTypePlotFile)
---
+++ Visualisation/runTestZonePlots.sh
@@ -0,0 +1,8 @@
+#python CreateZoneMangroveExtentPlots.py --zones /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/VisualZones/VisualZoneTest.shp --zonename FileName --plotpath /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/VisualZones/TestPlots --data /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/MangChangePVFC_V2_1987_to_2016.pkl.gz --grid /Users/pete/Temp/AustralianMangroves/AustraliaSqGrid_MangroveRegionsV1_ExtraV2Info.shp --gridid GridID --tmp /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/tmp
+
+
+#python CreateZoneMangroveExtentPlots.py --zones /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/VisualZones/Mangrove_coastal_regions.shp --zonename Zone --plotpath /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/VisualZones/CoastalRegionsPlots --data /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/MangChangePVFC_V2.1_1987_to_2016.pkl.gz --grid /Users/pete/Temp/AustralianMangroves/AustraliaSqGrid_MangroveRegionsV1_ExtraV2Info.shp --gridid GridID --tmp /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/tmp
+
+
+python CreateZoneMangroveExtentPlots.py --zones /Users/pete/Temp/AustralianMangroves/CycloneAnalysis/MangroveMajorStormTracksAustraliaDissolve_epsg3577_buffer_multipart.shp --zonename FileName --plotpath /Users/pete/Temp/AustralianMangroves/CycloneAnalysis/Plots_V2.1 --data /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/MangChangePVFC_V2.1_1987_to_2016.pkl.gz --grid /Users/pete/Temp/AustralianMangroves/AustraliaSqGrid_MangroveRegionsV1_ExtraV2Info.shp --gridid GridID --tmp /Users/pete/Temp/AustralianMangroves/MangChangePVFC_V2/tmp
+
